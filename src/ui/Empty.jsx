d6a22edfbc4ae1639a3f5708111c7e14d16b257a
DayOneAI <bambathiam88@gmail.com>
2024-02-22 22:15:44 +0100

    feat: ✨Filtering & Sorting Cabins #38

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 function Empty({ resource }) {
   return <p>No {resource} could be found.</p>;
 }
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +34,36 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+//eslint-disable-next-line
+function Filter({ filterField, options }) {
+  
+  const [searchParams, setSearchParams] = useSearchParams();
+  //eslint-disable-next-line
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
+
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {/* eslint-disable-next-line */}
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
